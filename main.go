261e8c848a85b334fd5d4b7e352fd199fda09659
fabs <fabiant941@gmail.com>
2025-07-01 15:46:39 -0700

    Web crawler respects disallowed paths

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -20,11 +21,13 @@ type URLMetaData struct {
 	Depth         uint8
 }
 
+// TODO: Consider turning the allow and disallow into a trie
 type DomainMetaData struct {
 	Domain        string
 	LastCrawlTime time.Time
 	UserAgent     string
 	Disallow      []string
+	Allow         []string
 	CrawlDelay    time.Duration
 }
 
@@ -53,10 +56,39 @@ func MakeWebCrawler(userAgent string) *WebCrawler {
 	return &wc
 }
 
-func parseRobotsTxt(body io.Reader, userAgent string) ([]string, time.Duration) {
+// Quick workaround for now
+func matchesPatterns(path string, rule string) bool {
+	if rule == "" {
+		return false
+	}
+
+	if strings.HasSuffix(rule, "*") {
+		prefix := strings.TrimSuffix(rule, "*")
+		return strings.HasPrefix(path, prefix)
+	}
+
+	return strings.HasPrefix(path, rule)
+}
+
+func (dm *DomainMetaData) IsPathAllowed(link string) bool {
+	for _, rule := range dm.Allow {
+		if matchesPatterns(link, rule) {
+			return true
+		}
+	}
+	for _, rule := range dm.Disallow {
+		if matchesPatterns(link, rule) {
+			return false
+		}
+	}
+	return true
+}
+
+func parseRobotsTxt(body io.Reader, userAgent string) ([]string, []string, time.Duration) {
 	scanner := bufio.NewScanner(body)
 
 	var disallowRules []string
+	var allowRules []string
 	var crawlDelay time.Duration
 
 	var allUserAgentRules = make(map[string][]string)
@@ -86,6 +118,10 @@ func parseRobotsTxt(body io.Reader, userAgent string) ([]string, time.Duration) 
 			for _, agent := range currentUserAgents {
 				allUserAgentRules[agent] = append(allUserAgentRules[agent], value)
 			}
+		case "allow":
+			for _, agent := range currentUserAgents {
+				allUserAgentRules[agent] = append(allUserAgentRules[agent], value)
+			}
 		case "crawl-delay":
 			delay, err := strconv.Atoi(value)
 			if err == nil {
@@ -102,7 +138,7 @@ func parseRobotsTxt(body io.Reader, userAgent string) ([]string, time.Duration) 
 		if delay, ok := allCrawlDelays[strings.ToLower(userAgent)]; ok {
 			crawlDelay = delay
 		}
-		return disallowRules, crawlDelay
+		return disallowRules, allowRules, crawlDelay
 	}
 
 	// Fallback to wildcard
@@ -111,10 +147,10 @@ func parseRobotsTxt(body io.Reader, userAgent string) ([]string, time.Duration) 
 		if delay, ok := allCrawlDelays["*"]; ok {
 			crawlDelay = delay
 		}
-		return disallowRules, crawlDelay
+		return disallowRules, allowRules, crawlDelay
 	}
 
-	return disallowRules, crawlDelay // Return empty rules and zero delay if no match
+	return disallowRules, allowRules, crawlDelay // Return empty rules and zero delay if no match
 }
 
 func (wc *WebCrawler) ProcessRobotFile(link string) (DomainMetaData, error) {
@@ -133,6 +169,7 @@ func (wc *WebCrawler) ProcessRobotFile(link string) (DomainMetaData, error) {
 	if resp.StatusCode == http.StatusNotFound {
 		return DomainMetaData{
 			Disallow:   []string{},
+			Allow:      []string{},
 			CrawlDelay: 1 * time.Second, // Default politeness
 		}, nil
 	}
@@ -141,7 +178,7 @@ func (wc *WebCrawler) ProcessRobotFile(link string) (DomainMetaData, error) {
 		return DomainMetaData{}, fmt.Errorf("failed to fetch robots.txt: %s", resp.Status)
 	}
 
-	disallowRules, crawlDelay := parseRobotsTxt(resp.Body, wc.userAgent)
+	disallowRules, allowRules, crawlDelay := parseRobotsTxt(resp.Body, wc.userAgent)
 
 	u, err := url.Parse(link)
 	if err != nil {
@@ -151,6 +188,7 @@ func (wc *WebCrawler) ProcessRobotFile(link string) (DomainMetaData, error) {
 	return DomainMetaData{
 		Domain:     u.Host,
 		Disallow:   disallowRules,
+		Allow:      allowRules,
 		CrawlDelay: crawlDelay,
 	}, nil
 }
@@ -221,9 +259,16 @@ func (wc *WebCrawler) ExtractLinks(body []byte, baseURL string) []string {
 }
 
 func main() {
+	file, err := os.Create("crawl_log.txt")
+	if err != nil {
+		fmt.Println("Error creating log file:", err)
+		return
+	}
+	defer file.Close()
+
 	CRAWL_DEQUEUE = append(CRAWL_DEQUEUE, CrawlJob{"https://en.wikipedia.org/wiki/Web_crawler", 0})
 	wc := MakeWebCrawler("fabs_bot/1.0")
-	for len(CRAWL_DEQUEUE) > 0 {
+	for i := 0; i < 6 && len(CRAWL_DEQUEUE) > 0; i++ {
 		job := CRAWL_DEQUEUE[0]
 		CRAWL_DEQUEUE = CRAWL_DEQUEUE[1:]
 
@@ -235,18 +280,40 @@ func main() {
 		if _, ok := DOMAIN_METADATA_MAP[domain.Host]; !ok {
 			domain_metadata, err := wc.ProcessRobotFile(domain.Scheme + "://" + domain.Host + "/robots.txt")
 			if err != nil {
-                continue
-            }
-            DOMAIN_METADATA_MAP[domain.Host] = domain_metadata
-            fmt.Printf("Successfully processed robots.txt for %s: %+v\n", domain.Host, domain_metadata)
-        }
+				continue
+			}
+			DOMAIN_METADATA_MAP[domain.Host] = domain_metadata
+		}
 
-        // body, err := wc.ProcessJob(job)
-        // if err != nil {
-        //  fmt.Printf("Error occured: %s\n", err)
-        //  return
-        // }
-        // fmt.Println(wc.ExtractLinks(body, job.Link))
+		if result := DOMAIN_METADATA_MAP[domain.Host]; !result.IsPathAllowed(domain.Path) {
+			fmt.Fprintf(file, "Request to %s is not allowed\n", job.Link)
+			continue
+		}
+
+		if _, ok := URL_METADATA_MAP[job.Link]; !ok {
+			URL_METADATA_MAP[job.Link] = URLMetaData{
+				Link:          job.Link,
+				LastCrawlTime: time.Now(),
+				Depth:         1,
+			}
+		} else {
+			continue
+		}
+
+		body, err := wc.ProcessJob(job)
+		if err != nil {
+			fmt.Printf("Error occured: %s\n", err)
+			return
+		}
+
+		links := wc.ExtractLinks(body, job.Link)
+
+		fmt.Fprintf(file, "Crawled: %s\n", job.Link)
+		for _, link := range links {
+			fmt.Fprintf(file, "\tfound: %s\n", link)
+			CRAWL_DEQUEUE = append(CRAWL_DEQUEUE, CrawlJob{link, 0})
+		}
+		time.Sleep(10 * time.Second)
 	}
 }
 
